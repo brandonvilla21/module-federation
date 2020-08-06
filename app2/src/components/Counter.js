56108f91bf9405bb09f0783b25a551f87fa81417
Brandon Villa <bornofos@gmail.com>
2020-08-05 20:06:40 -0500

    Include second app exposing the Counter component

---
+++ app2/src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Counter(props) {
+  return (
+    <>
+      <p>Count: {props.count}</p>
+      <button onClick={props.onIncrement}>Increment</button>
+      <button onClick={props.onDecrement}>Decrement</button>
+    </>
+  );
+}
